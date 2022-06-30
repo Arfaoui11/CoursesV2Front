4f2aacb14a7abe392e6c7c5243da69ed7eff9109
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-06-30 01:19:57 +0100

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,15 +28,6 @@ import {CalendarFrontComponent} from "./CoursesSpace/calendar-front/calendar-fro
 import {QestionQuizCoursesComponent} from "./CoursesSpace/qestion-quiz-courses/qestion-quiz-courses.component";
 import {E404Component} from "./FontEnd/e404/e404.component";
 import {CallVideoComponent} from "./CoursesSpace/call-video/call-video.component";
-import {AddPartnerInstitutionComponent} from "./backup/ExchangeStudents/add-partner-institution/add-partner-institution.component";
-import {ListOfPartnersComponent} from "./backup/ExchangeStudents/list-of-partners/list-of-partners.component";
-import {AddAppointmentComponent} from "./backup/add-appointment/add-appointment.component";
-import {AppointmentListComponent} from "./backup/appointment-list/appointment-list.component";
-import {AddComplaintComponent} from "./backup/add-complaint/add-complaint.component";
-import {ComplaintListComponent} from "./backup/complaint-list/complaint-list.component";
-import {UpdateComplaintComponent} from "./backup/update-complaint/update-complaint.component";
-import {UpdateAppointmentComponent} from "./backup/update-appointment/update-appointment.component";
-import {ListTopicComponent} from "./backup/Forum/list-topic/list-topic.component";
 import {TeamComponent} from "./CoursesSpace/team/team.component";
 import {RegisterComponent} from "./BackEnd/register/register.component";
 
@@ -106,41 +97,7 @@ const routes: Routes =
 
         ]
       }
-      ,
-      {
-        path:'Exchange-Student-Management',
-        children: [
-          { path: 'addUniversity', component: AddPartnerInstitutionComponent },
-          { path: 'ListUniversities', component: ListOfPartnersComponent },
 
-
-        ]
-
-      },
-      {
-        path: 'helpSpace-Management',
-        children: [
-
-
-          { path: 'addAppointment', component: AddAppointmentComponent },
-          { path: 'ListAppointment', component: AppointmentListComponent },
-          { path: 'addComplaint', component: AddComplaintComponent },
-          { path: 'ListComplaint', component: ComplaintListComponent } ,
-          { path: 'updateComplaint/:id', component: UpdateComplaintComponent },
-          { path: 'updateAppointment/:id', component: UpdateAppointmentComponent },
-
-        ]
-
-      },
-
-      {
-        path: 'Forum-management',
-        children: [
-
-          { path: 'ListTopic', component: ListTopicComponent },
-
-        ]
-      },
   ]
   }
 ];
