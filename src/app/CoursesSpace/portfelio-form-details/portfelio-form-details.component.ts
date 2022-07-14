149689c418f79ff0d536ac81535aeaaadc1e987f
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-07-14 13:40:28 +0100

    new 07/07/2022

--- src/app/CoursesSpace/portfelio-form-details/portfelio-form-details.component.ts
+++ src/app/CoursesSpace/portfelio-form-details/portfelio-form-details.component.ts
@@ -192,9 +192,9 @@ export class PortfelioFormDetailsComponent implements OnInit {
     this.pathUrl = 'http://localhost:4000/api/video/'+ path.slice(37);
 
 
-  /*  this.serviceForm.getVideo(path.slice(37)).subscribe( data => {
+    this.serviceForm.getVideo(path.slice(37)).subscribe( data => {
       console.log(data)
-    });*/
+    });
 
   }
 
